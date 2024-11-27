bad8330cf5c3c2adfa3044bb81c38579c70c83f9
pseudoureka <messiah7179@gmail.com>
2024-11-27 22:48:08 +0900

    gitHub 업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Input from "./Input";
+
+const Container = styled.div`
+  ${Input} {
+    margin: 8px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h2>Size</h2>
+      <Input size="small" />
+      <Input size="medium" />
+      <Input size="large" />
+      <h2>Round</h2>
+      <Input round />
+      <h2>Error</h2>
+      <Input error />
+    </Container>
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const SIZES = {
+  large: 24,
+  medium: 20,
+  small: 16,
+};
+
+const Input = styled.input`
+  font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
+  border: 2px solid ${({ error }) => (error ? `#f44336` : `#eeeeee`)};
+  border-radius: ${({ round }) => (round ? `9999px` : `4px`)};
+  outline: none;
+  padding: 16px;
+
+  &:focus {
+    border-color: ${({ error }) => (error ? `#f44336` : `#7760b4`)};
+  }
+`;
+
+export default Input;
